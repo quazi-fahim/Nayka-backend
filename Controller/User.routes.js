159ec4fa4d5daee99d48b1d4c6784d27a88c6d7b
Quazi fahim <fahimahmad.4388@gmail.com>
2024-12-12 12:14:22 +0530

    backtend

--- Controller/User.routes.js
+++ Controller/User.routes.js
@@ -34,7 +34,7 @@ router.post("/register", async (req, res) => {
 });
 
 // Login
-router.post("/login", async (req, res) => {
+router.post("/signin", async (req, res) => {
   console.log("Register endpoint hit");
   try {
     const { email, password } = req.body;
